71fd7c4f6974ce75ec627004b39347969d13e654
Martin Robinson <0x4d52@gmail.com>
2016-10-31 19:07:36 +0000

    Added an typical example of using WeakReference.

--- adc2016/05_WeakReference/Source/MainComponent.h
+++ adc2016/05_WeakReference/Source/MainComponent.h
@@ -3,16 +3,108 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-class MainContentComponent   : public Component
+class ComponentColourHighlighter   : private Timer
 {
 public:
-    //==============================================================================
+    ComponentColourHighlighter()
+    {
+        startTimer (10);
+    }
+    
+    ~ComponentColourHighlighter()
+    {
+        for (int i = highlightedComponents.size(); --i >= 0;)
+        {
+            Holder* const highlightedComponentHolder = highlightedComponents.getUnchecked (i);
+            
+            if (Component* const originalComp = highlightedComponentHolder->comp.get())
+                originalComp->setColour (highlightedComponentHolder->colourId,
+                                         highlightedComponentHolder->originalColour);
+        }
+    }
+    
+    void highlightComponent (Component* comp, int colourId, Colour highlightColour, int numTicks)
+    {
+        Holder* highlightedComponentHolder = findHolderForComponentAndColourId (comp, colourId);
+        
+        if (highlightedComponentHolder == nullptr)
+        {
+            highlightedComponentHolder = new Holder();
+            highlightedComponentHolder->originalColour = comp->findColour (colourId);
+            highlightedComponentHolder->comp           = comp;
+            highlightedComponentHolder->colourId       = colourId;
+            highlightedComponents.add (highlightedComponentHolder);
+        }
+        
+        highlightedComponentHolder->ticksLeft = jmax (1, numTicks);
+        
+        comp->setColour (colourId, highlightColour);
+    }
+    
+private:
+    struct Holder
+    {
+        Colour originalColour;
+        WeakReference<Component> comp;
+        int colourId;
+        int ticksLeft;
+    };
+
+    Holder* findHolderForComponentAndColourId (Component* comp, int colourId)
+    {
+        for (int i = highlightedComponents.size(); --i >= 0;)
+        {
+            Holder* const highlightedComponentHolder = highlightedComponents.getUnchecked (i);
+
+            if (highlightedComponentHolder->comp.get() == comp
+                &&  highlightedComponentHolder->colourId == colourId)
+                return highlightedComponentHolder;
+        }
+        
+        return nullptr;
+    }
+    
+    void timerCallback() override
+    {
+        for (int i = highlightedComponents.size(); --i >= 0;)
+        {
+            Holder* const highlightedComponentHolder = highlightedComponents.getUnchecked (i);
+            
+            if (--highlightedComponentHolder->ticksLeft == 0)
+            {
+                if (Component* const originalComp = highlightedComponentHolder->comp.get())
+                    originalComp->setColour (highlightedComponentHolder->colourId,
+                                             highlightedComponentHolder->originalColour);
+                    
+                highlightedComponents.remove (i);
+            }
+        }
+    }
+    
+    //==========================================================================
+    
+    OwnedArray<Holder> highlightedComponents;
+};
+
+//==============================================================================
+
+class MainContentComponent   : public Component,
+                               private Slider::Listener,
+                               private Button::Listener
+{
+public:
+    //==========================================================================
     MainContentComponent()
     {
         addAndMakeVisible (slider1 = new Slider());
         addAndMakeVisible (slider2 = new Slider());
         addAndMakeVisible (button1 = new TextButton ("Button 1"));
         addAndMakeVisible (button2 = new TextButton ("Button 2"));
+        
+        slider1->addListener (this);
+        slider2->addListener (this);
+        button1->addListener (this);
+        button2->addListener (this);
         
         setSize (400, 300);
     }
@@ -38,12 +130,25 @@ public:
     }
 
 private:
+    void sliderValueChanged (Slider* slider) override
+    {
+        highlighter.highlightComponent (slider, Slider::thumbColourId, Colours::red, 50);
+    }
+    
+    void buttonClicked (Button* button) override
+    {
+        highlighter.highlightComponent (button, TextButton::textColourOffId, Colours::red, 50);
+    }
+    
+    //==========================================================================
     ScopedPointer<Slider>     slider1;
     ScopedPointer<Slider>     slider2;
     ScopedPointer<TextButton> button1;
     ScopedPointer<TextButton> button2;
     
-    //==============================================================================
+    ComponentColourHighlighter highlighter;
+    
+    //==========================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
 
