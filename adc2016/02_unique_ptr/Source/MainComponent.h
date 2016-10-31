9048b3378d8ca388a447ada183aa9318b12273d4
Martin Robinson <0x4d52@gmail.com>
2016-10-31 15:31:41 +0000

    Simple replacement of ScopedPointer with unique_ptr doesn't work.

---
+++ adc2016/02_unique_ptr/Source/MainComponent.h
@@ -0,0 +1,51 @@
+#ifndef MAINCOMPONENT_H_INCLUDED
+#define MAINCOMPONENT_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class MainContentComponent   : public Component
+{
+public:
+    //==============================================================================
+    MainContentComponent()
+    {
+        addAndMakeVisible (slider1 = new Slider());
+        addAndMakeVisible (slider2 = new Slider());
+        addAndMakeVisible (button1 = new TextButton ("Button 1"));
+        addAndMakeVisible (button2 = new TextButton ("Button 2"));
+        
+        setSize (400, 300);
+    }
+    
+    ~MainContentComponent()
+    {
+    }
+
+    void paint (Graphics& g) override
+    {
+        g.setColour (Colours::wheat);
+        g.fillRect (getLocalBounds());
+    }
+    
+    void resized() override
+    {
+        Rectangle<int> area (getLocalBounds().reduced (8));
+        
+        slider1->setBounds (area.removeFromTop (30).reduced (2));
+        slider2->setBounds (area.removeFromTop (30).reduced (2));
+        button1->setBounds (area.removeFromTop (30).reduced (2));
+        button2->setBounds (area.removeFromTop (30).reduced (2));
+    }
+
+private:
+    std::unique_ptr<Slider>     slider1;
+    std::unique_ptr<Slider>     slider2;
+    std::unique_ptr<TextButton> button1;
+    std::unique_ptr<TextButton> button2;
+    
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+
+#endif  // MAINCOMPONENT_H_INCLUDED
